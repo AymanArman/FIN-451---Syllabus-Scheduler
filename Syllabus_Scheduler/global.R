d58a38ab4600f8bf12ef8a3a0005c6b06462467b
Carter Vereschagin <carter.vereschagin@gmail.com>
2026-02-07 15:37:13 -0700

    added courses.json to global.R

--- Syllabus_Scheduler/global.R
+++ Syllabus_Scheduler/global.R
@@ -1,12 +1,6 @@
+library(jsonlite)
 
-
-
-
-# Web scrape https://apps.ualberta.ca/catalogue/course/fin to pull all available FIN course codes
-
-# scraping script goes here
-
-
+course_data <- jsonlite::fromJSON("./web_scraper/courses.json")
 
 # https://apps.ualberta.ca/catalogue/course/fin/201 change course number at the end of this link and loop dynamically to scrape each number from above df ^
 # Search for: <div class="alert alert-warning" role="alert"> to check if course is offered (or look for text / use xpath) if exists then move on otherwise scrape syllabus
