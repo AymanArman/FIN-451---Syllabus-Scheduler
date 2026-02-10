3f16a23296fe731a0ca9a4a83ec30bc2445faa29
Duncan-McNabb <dbmcnabb@ualberta.ca>
2026-02-10 10:26:54 -0700

    storing

--- Syllabus_Scheduler/server.R
+++ Syllabus_Scheduler/server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(lubridate)
 library(pdftools)
@@ -14,6 +5,9 @@ library(tesseract)
 library(tibble)
 
 server <- function(input, output, session) {
+
+  # Page switching state
+  current_page <- reactiveVal("upload")
 
   output$course_number_dropdown <- renderUI({
     req(input$course)
@@ -38,48 +32,11 @@ server <- function(input, output, session) {
     actionButton("submit_button","Submit")
   })
 
-
-  # observeEvent(input$course, {
-  #   course_number_choices <- course_data %>%
-  #     dplyr::filter(course_code == input$course) %>%
-  #     pull(course_number)
-  #   updateSelectInput("course_number_pick", "Select Course Number", course_number_choices)
-  # })
-  #
-  # observeEvent(input$course_number_pick, {
-  #   course_id_choices <- course_data %>%
-  #     dplyr::filter(course_code == input$course,
-  #                   course_number == input$course_number_pick) %>%
-  #     pull(course_ids) %>%
-  #     unlist()
-  #   updateSelectInput("course_id_pick", "Select Course ID", course_id_choices)
-  # })
-
-
-  ## Checking to see how long it takes to dynamically download and run OCR while user is using the app
-
+  ## Catalog syllabus download and OCR
   observeEvent(input$submit_button, {
     base_url <- "https://apps.ualberta.ca/catalogue/syllabus/download/"
-
     download_url <- paste0(base_url, "1940", input$course_id)
     file_name <- paste0(input$course_id, ".pdf")
-
-    # base_dir <- file.path(getwd(), "syllabus_downloads")
-    # course_dir <- file.path(base_dir, input$course)
-    # course_number_dir <- file.path(course_dir, input$course_number_pick)
-    # file_path <- file.path(course_number_dir, file_name)
-    #
-    # if (!dir.exists(course_dir)) {
-    #   dir.create(course_dir)
-    # }
-    #
-    # if (!dir.exists(course_number_dir)) {
-    #   dir.create(course_number_dir)
-    # }
-    #
-    # if (!file.exists(file_path)) {
-    #   download.file(download_url, file_path)
-    # }
 
     print(Sys.time())
     base_dir <- file.path(getwd())
@@ -89,8 +46,6 @@ server <- function(input, output, session) {
     if (!dir.exists(cache_dir)) {
       dir.create(cache_dir)
     }
-
-    file_path
 
     download.file(download_url, file_path, mode ="wb")
 
@@ -104,131 +59,44 @@ server <- function(input, output, session) {
     print(pdf_text)
     print(paste0("DONE: ",Sys.time()))
 
-    ## Downloading and converting to text with OCR after submit
-    ## currently takes ~10.2 seconds before any text matching.
-
-  })
-
-
-#######################################################################
-
-  current_page <- reactiveVal("upload")
-
-  default_start <- Sys.Date()
-  default_end <- default_start %m+% months(4)
-
-  output$dynamic_page <- renderUI({
-    if (current_page() == "upload") {
-      tagList(
-        mainPanel(
-        titlePanel("Syllabus Upload and Semester Dates"),
-        selectInput(
-          inputId = "Fac_selection",
-          label = "Select Your Faculty Here",
-          choices = c("FIN", "ACCTG")
-          ),
-        selectInput(
-          inputId = "Course_selection",
-          label = "Select Your Course Number Here",
-          choices = NULL
-        ),
-        dateRangeInput("semester_dates",
-                       "Select Your Semester Start and End Dates",
-                       start = default_start,
-                       end = default_end),
-        numericInput("table_page",
-                     "Enter Page Containing Course Schedule",
-                     value = 1,
-                     min = 1,
-                     max = 20),
-        actionButton("submit_btn", "Submit")
-        )
-      )
-    } else {
-      tagList(
-        sidebarLayout(
-          sidebarPanel(
-              tableOutput("calendar_tab")
-            ),
-            mainPanel(
-              titlePanel("Calender"),
-              verbatimTextOutput("extracted_text"),
-              actionButton("back_btn", "Go Back")
-            )
-          )
-        )
-    }
-  })
-  observeEvent(input$Fac_selection, {
-    if (input$Fac_selection == "FIN") {
-      updateSelectInput(session, "Course_selection",
-                        choices = c("201", "312", "322", "413", "414", "415", "416", "418", "430", "434", "436A", "436B", "440", "442", "445", "449", "450", "451", "452", "455", "460", "473", "480", "488", "495", "496", "497"))
-    } else if (input$Fac_selection == "ACCTG") {
-      updateSelectInput(session, "Course_selection",
-                        choices = c("200", "211", "222", "312", "314", "315", "324", "416", "418", "426", "432", "437", "456", "463", "467", "468", "480", "481", "488", "495", "496", "497"))
-    }
-  })
-
-
-
-
-  syllabus_data <- eventReactive(input$submit_btn, {
-    req(input$Syllabus_Upload)
-    req(input$table_page)
-    file_path <- input$Syllabus_Upload$datapath
-    table <- tabulapdf::extract_tables(file_path,
-                                       pages = table_page,
-                                       method = "stream")
-  })
-
-
-  calendar_init <- eventReactive(input$semester_dates, {
-
-    start_date <- as.Date(input$semester_dates[1])
-    end_date <- as.Date(input$semester_dates[2])
-
-    tibble::tibble(date = format(seq.Date(from = start_date,
-                                               to = end_date,
-                                               by = 1), "%Y-%m-%d"),
-                   activity = "")
-
-  })
-
-
-
-  nlp_process <- reactive({
-
-
-    #can do nlp process in here
-
-    calendar <- calendar_init()
-
-    print(calendar)
-
-
-
-
-
-  })
-
-
-
-
-  output$extracted_text <- renderText({
-    syllabus_data()
-  })
-
-  output$calendar_tab <- renderTable({
-    req(current_page() == "results")
-    nlp_process()
-  })
-
-
-  observeEvent(input$submit_btn, {
+    # Switch to results page
     current_page("results")
   })
 
+  # Page switch observer for back button
   observeEvent(input$back_btn, {
     current_page("upload")
+  })
+
+  # Dynamic page output
+  output$dynamic_page <- renderUI({
+    if (current_page() == "upload") {
+      fluidRow(
+        column(width = 8,
+               wellPanel(
+                 h3("Class Selection"),
+                 selectInput("course","Select Course Code", course_data$course_code),
+                 uiOutput("course_number_dropdown"),
+                 uiOutput("course_id_dropdown"),
+                 uiOutput("submit_button")
+               )
+        ),
+        column(width = 8,
+               wellPanel(
+                 h3("Upload PDF"),
+                 fileInput("file_input","Upload a PDF", accept = ".pdf")
+               )
+        )
+      )
+    } else {
+      fluidRow(
+        column(width = 12,
+               wellPanel(
+                 h3("Results"),
+                 actionButton("back_btn", "Go Back")
+               )
+        )
+      )
+    }
   })
 }
--- Syllabus_Scheduler/ui.R
+++ Syllabus_Scheduler/ui.R
@@ -1,63 +1,9 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(lubridate)
 library(bslib)
 
-# page_fillable(
-#   titlePanel("Syllabus Event Creator"),
-#   layout_columns(
-#     card(
-#       card_header("Select Course Code"),
-#       selectInput("course","Select Course Code", course_data$course_code),
-#       uiOutput("course_number_dropdown"),
-#       uiOutput("course_id_dropdown"),
-#       uiOutput("submit_button")
-#       ),
-#     card(
-#       card_header("Upload PDF"),
-#       fileInput("file_input","Upload a PDF", accept = ".pdf")
-#     )
-#   )
-# )
-
-###########################################
-
 fluidPage(
-  fluidRow(
-    column(width = 6,
-           wellPanel(
-             h3("Class Selection"),
-             #p("If your class isn't shown, upload a pdf of your syllabus"),
-             selectInput("course","Select Course Code", course_data$course_code),
-             uiOutput("course_number_dropdown"),
-             uiOutput("course_id_dropdown"),
-             uiOutput("submit_button")
-           )
-    ),
-    column(width = 6,
-           wellPanel(
-             h3("Upload PDF"),
-             #p("Insert your own syllabus and have us analyze it for you."),
-             fileInput("file_input","Upload a PDF", accept = ".pdf")
-           )
-    )
+  mainPanel(
+    uiOutput("dynamic_page")
   )
 )
-
-
-##########################################3
-#
-# fluidPage(
-#   mainPanel(
-#     uiOutput("dynamic_page")
-#   )
-# )
-
