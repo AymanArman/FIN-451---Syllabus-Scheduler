67d5fd4d1b79be54c4e1c160c93f4596e79d9f48
Carter Vereschagin <carter.vereschagin@gmail.com>
2026-02-11 21:32:55 -0700

    changed course_id to sections with dates and times

--- Syllabus_Scheduler/server.R
+++ Syllabus_Scheduler/server.R
@@ -117,15 +117,8 @@ server <- function(input, output, session) {
                     course_number == input$course_number_pick) %>%
       dplyr::mutate(course_section_choice = paste0(course_section, " ", course_dates, " ", course_times)) %>%
       pull(course_section_choice)
-    selectInput("course_id", "Select Course ID", choices = course_section_choices)
+    selectInput("course_id", "Select Course Section", choices = course_section_choices)
   })
-
-  # course_section_choices <- course_data %>%
-  #   dplyr::filter(course_code == 'FIN',
-  #                 course_number == '201') %>%
-  #   dplyr::mutate(course_section_choice = paste0(course_section, " ", course_dates, " ", course_times)) %>%
-  #   pull(course_section_choice) %>%
-  #   unlist()
 
   output$submit_button <- renderUI({
     req(input$course_id)
